0ff6c28e69d181a2a22d89985a94d98fa136bebb
kamol <kamol.somkrai@gmail.com>
2024-12-18 10:50:41 +0700

    adjust some variable

--- app/components/(measure)/Measure1.tsx
+++ app/components/(measure)/Measure1.tsx
@@ -16,7 +16,7 @@ const Measure1: React.FC<Props> = ({ formData, handleChange }) => (
       <textarea
         id="measure1_1"
         name="measure1_1"
-        value={formData.measure1_1 || ''}
+        value={formData?.measure1_1 || ''}
         onChange={handleChange}
         className="border border-gray-300 rounded-md px-3 py-2 w-full focus:ring-blue-500 focus:border-blue-500"
       />
@@ -29,7 +29,7 @@ const Measure1: React.FC<Props> = ({ formData, handleChange }) => (
       <textarea
         id="measure1_2"
         name="measure1_2"
-        value={formData.measure1_2 || ''}
+        value={formData?.measure1_2 || ''}
         onChange={handleChange}
         className="border border-gray-300 rounded-md px-3 py-2 w-full focus:ring-blue-500 focus:border-blue-500"
       />
