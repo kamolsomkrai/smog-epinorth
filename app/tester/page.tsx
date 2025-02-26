afcbeee32864934184801fbf04572eaec260a432
kamol <kamol.somkrai@gmail.com>
2025-02-26 14:31:54 +0700

    test

--- app/tester/page.tsx
+++ app/tester/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Testcom from '../components/(global)/Testcom'
 
 const page = () => {
   return (
-    <div><Testcom name="asdasdasd" typename="text" /></div>
+    <div>page</div>
   )
 }
 
